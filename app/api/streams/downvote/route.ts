d5192f2fa278430c0e8189b4013cbc155b8f29ba
Sahil-Sam <itsmesahil357@gmail.com>
2024-10-24 16:05:51 +0530

    update

--- app/api/streams/downvote/route.ts
+++ app/api/streams/downvote/route.ts
@@ -1,48 +1,59 @@
 import { prismaClient } from "@/app/lib/db";
-import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+import { getServerSession } from "next-auth"
+import { NextRequest, NextResponse } from "next/server"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Stream } from "stream";
+import { z } from "zod"
+
+
+
 
 const UpvoteSchema = z.object({
     streamId: z.string(),
-});
+})
 
-export async function POST(req: NextRequest) {
+
+export async function POST(req: NextRequest ) {
     const session = await getServerSession();
 
     const user = await prismaClient.user.findFirst({
         where: {
-            email: session?.user?.email ?? "",
-        },
+            email: session?.user?.email ?? ""
+        }
     });
 
     if (!user) {
-        return NextResponse.json(
-            { message: "Unauthenticated" },
-            { status: 403 }
-        );
+        return NextResponse.json({
+            message: "Unauthenticated"
+        },{
+            status: 403
+        }
+    )
     }
 
     try {
-        const data = UpvoteSchema.parse(await req.json());
+    const data = UpvoteSchema.parse(await req.json());
+     await prismaClient.upvote.delete({
+        where:  {
+            userId_streamId: {
+                userId: user.id,
+                streamId:data.streamId
+            }
+        }
+     })
 
-        await prismaClient.upvote.delete({
-            where: {
-                userId_streamId: {
-                    userId: user.id,
-                    streamId: data.streamId,
-                },
-            },
-        });
+     return NextResponse.json({
+        message: "Done!"
+    })
 
-        return NextResponse.json({
-            message: "Done!",
-        });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (e) {
-        return NextResponse.json(
-            { message: "Error while upvoting" },
-            { status: 403 }
-        );
+    } catch(e){
+        return NextResponse.json({
+            message: "Error while upvoting"
+        },{
+            status: 403
+        }
+    )
     }
+    
 }
